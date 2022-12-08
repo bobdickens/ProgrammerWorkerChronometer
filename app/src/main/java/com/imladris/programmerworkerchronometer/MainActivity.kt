db012abc49f311d90c907314684ab976e25f69a4
Anton <m.anton.android@expresspanda.ru>
2022-12-08 15:12:14 +0300

    hide navigation bar

--- app/src/main/java/com/imladris/programmerworkerchronometer/MainActivity.kt
+++ app/src/main/java/com/imladris/programmerworkerchronometer/MainActivity.kt
@@ -24,7 +24,15 @@ class MainActivity : AppCompatActivity() {
                 startCountDownTimer(3600000)
             }
         }
+        window.decorView.apply {
+            // Hide both the navigation bar and the status bar.
+            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+            // a general rule, you should design your app to hide the status bar whenever you
+            // hide the navigation bar.
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
     }
+
 
     private fun startCountDownTimer(timeMillis: Long){
         binding.textrelax.visibility = View.INVISIBLE
